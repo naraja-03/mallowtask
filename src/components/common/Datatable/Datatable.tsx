e041c716f943a6c810cc405108085c10e56a49b2
naraja-03 <153056175+naraja-03@users.noreply.github.com>
2024-11-24 16:58:35 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/components/common/Datatable/Datatable.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import React from "react";
+import { Table } from "antd";
+import { DataType, getColumnsConstant } from "@/constants/columnConstant";
+import { useAppDispatch } from "@/hooks/redux";
+import { updateModalOpen, updateSelectedUser } from "@/slice/appSlice";
+
+interface DataTableProps {
+  userList: DataType[];
+  totalUsers: number;
+  currentPage: number;
+  onPageChange: (page: number) => void;
+  onDelete: (id: string) => void;
+}
+
+const DataTableComponent: React.FC<DataTableProps> = ({
+  userList,
+  totalUsers,
+  currentPage,
+  onPageChange,
+  onDelete,
+}) => {
+  const dispatch = useAppDispatch();
+  const handleEditUser = (user: any) => {
+    dispatch(updateModalOpen(true));
+    dispatch(updateSelectedUser(user));
+  };
+  const handleDeleteUser = (user: any) => {
+    dispatch(updateSelectedUser(user));
+    onDelete(user.id)
+    
+  };
+  return (
+    <Table<DataType>
+      columns={getColumnsConstant({
+        onEdit: handleEditUser,
+        onDelete: handleDeleteUser,
+      })}
+      dataSource={userList}
+      pagination={{
+        current: currentPage,
+        total: totalUsers,
+        pageSize: 4,
+        onChange: onPageChange,
+      }}
+      rowKey="id"
+    />
+  );
+};
+
+export default DataTableComponent;
